4581c03b3ffd1356e18470d33709fc9db147be91
Nits-007 <122176311+Nits-007@users.noreply.github.com>
2025-02-09 06:32:07 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = 'postgresql://postgres:nits007@localhost:5432/sanjeevani'
+DATABASE_URL = 'postgresql://frosthacks_uy3a_user:Ias6xcVHKmU4dhSGbBP0tAzhiRQXFaWd@dpg-cujvsa0gph6c73bma9bg-a/frosthacks_uy3a'
 
 engine = create_engine(DATABASE_URL)
 
